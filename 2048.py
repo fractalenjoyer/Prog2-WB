f8b2cb546c09548d0babec4ae6c4db8661be98ad
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2022-09-19 17:08:13 +0200

    solved 2048

--- 2048.py
+++ 2048.py
@@ -1,43 +1,55 @@
-board = [
-    [2, 0, 0, 2], 
-    [4, 16, 8, 2], 
-    [2, 64, 32, 4], 
-    [1024, 1024, 64, 0]]
+board = [[*map(int,input().split())] for _ in range(4)]
 
 def left():
     for row in board:
-        zeros = len([*filter(lambda x: not x, row)])
-        row[:] = list([*filter(lambda x: x,row)] + [0]*zeros)
-        for i in range(3-zeros):
-            if row[i] == row[i+1]:
-                row[i] *= 2
-                row[i+1] = 0
-                left()
-
+        frow = [*filter(bool,row)]
+        for i in range(len(frow)-1):
+            if frow[i] == frow[i+1]:
+                frow[i] *= 2
+                frow[i+1] = 0
+        frow = [*filter(bool,frow)]
+        row[:] = frow + [0]*(4-len(frow))
+            
 def right():
     for row in board:
-        zeros = len([*filter(lambda x: not x, row)])
-        row[:] = list([0]*zeros + [*filter(lambda x: x,row)])
-        for i in range(3, zeros, -1):
-            if row[i] == row[i-1]:
-                row[i] *= 2
-                row[i-1] = 0
-                right()
+        frow = [*filter(bool,row)]
+        for i in range(len(frow)-1,0,-1):
+            if frow[i] == frow[i-1]:
+                frow[i] *= 2
+                frow[i-1] = 0
+        frow = [*filter(bool,frow)]
+        row[:] = [0]*(4-len(frow)) + frow
+    
+def up():
+    for col in range(4):
+        fcol = [*filter(bool,[row[col] for row in board])]
+        for i in range(len(fcol)-1):
+            if fcol[i] == fcol[i+1]:
+                fcol[i] *= 2
+                fcol[i+1] = 0
+        fcol = [*filter(bool,fcol)]
+        fcol = fcol + (4-len(fcol))*[0]
+        for row in range(4):
+            board[row][col] = fcol[row]   
 
+def down():
+    for col in range(4):
+        fcol = [*filter(bool,[row[col] for row in board])]
+        for i in range(len(fcol)-1,0,-1):
+            if fcol[i] == fcol[i-1]:
+                fcol[i] *= 2
+                fcol[i-1] = 0
+        fcol = [*filter(bool,fcol)]
+        fcol = (4-len(fcol))*[0] + fcol
+        for row in range(4):
+            board[row][col] = fcol[row]
 
+{
+    "0": left,
+    "1": up,
+    "2": right,
+    "3": down
+}[input()]()
 
-            
-
-
-
-# match input():
-#     case "0":
-#         left()
-#     case "1":
-#         up(),
-#     case "2":
-#         right(),
-#     case "3":
-#         down(),
-up()
-print(board)
+for row in board:
+    print(*row)
--- test.py
+++
@@ -1,8 +0,0 @@
-
-# acc = 0
-# for i in range(2**32):
-#     if f"{i:>b}"[0:2] == "11" or f"{i:>b}"[-2:] == "11":
-#         acc += 1
-
-# print(acc)
-
