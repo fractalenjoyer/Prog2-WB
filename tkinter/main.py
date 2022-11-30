1dee4a1396eca4fb14bceaadbb04a446b360937d
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2022-11-30 11:29:40 +0100

    made erasing better

--- tkinter/main.py
+++ tkinter/main.py
@@ -13,10 +13,21 @@ def key_handler(event):
         lbl["text"] += event.char
 
 def draw(event):
+    root.config(cursor="")
     canvas.create_oval(event.x, event.y, event.x+5, event.y+5, fill="black")
 
 def erase(event):
-    canvas.create_oval(event.x-10, event.y-10, event.x+10, event.y+10, fill="white", outline="white")
+    root.config(cursor="spraycan")
+    object = canvas.find_closest(event.x, event.y)
+    try:
+        coords = canvas.coords(object[0])
+    except IndexError:
+        return
+    if (coords[0] - event.x)**2 + (coords[1] - event.y)**2 < 400:
+        canvas.delete(object[0])
+
+    
+
 
 
 root.bind('<Key>', key_handler)
